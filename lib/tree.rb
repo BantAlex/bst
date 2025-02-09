fa560c0dbdf7fd00865ae534ee19c2eca7dafdf0
BantAlex <bantalex3@gmail.com>
2025-02-09 14:17:16 +0200

    Sort and remove duplicates of array

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,3 +1,14 @@
+require_relative 'node'
+
 class Tree
+
+ def initialize(array)
+    @root = build_tree(array)
+ end
+
+ def build_tree(array)
+   sorted_arr = array.sort.uniq
+   p sorted_arr
+ end
 
 end
