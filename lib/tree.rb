59411efbcfb50b49a92875590034735bf730ef65
BantAlex <bantalex3@gmail.com>
2025-02-17 13:02:07 +0200

    Fix #insert method

--- lib/tree.rb
+++ lib/tree.rb
@@ -11,12 +11,11 @@ class Tree
   def build_tree(arr,left,right)
       return nil if left > right
       root_index = (left + right)/2
-      root_node = Node.new(arr[root_index]) #!This should be a local variable! Otherwise it will update itself we're
+      root_node = Node.new(arr[root_index]) #*This should be a local variable! Otherwise it will update itself we're
       #!going to have only one output
-      root_node.left = build_tree(arr,left,root_index - 1) #We use the whole arr because the array is sorted.
-      #We don't need to split it in half every recur. Now we take the last index of the left array instead of whole.
+      root_node.left = build_tree(arr,left,root_index - 1) #*We use the whole arr because the array is sorted.
+      #*We don't need to split it in half every recur. Now we take the last index of the left array instead of whole.
       root_node.right = build_tree(arr,root_index + 1,right)
-
       return root_node
   end
 
@@ -26,29 +25,55 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
-  def insert(value)
+  def insert(node = @root,value)
     return "Wrong input!" if value.class != Integer
-    current_node = @root
-    new_node = Node.new(value)
 
-    until !current_node.left && !current_node.right
-      current_node = current_node.left if value < current_node.data
-      current_node = current_node.right if value > current_node.data
+   if value == node.data
+    p "This node already exists"
+    return node
+   end
+
+   if value > node.data
+    if !node.right #?there's probably a better way to deal wit that instead of nesting if statements
+      node.right = Node.new(value)
+    else
+      node.right = insert(node.right,value)
     end
+   end
 
-    p "This value already exists" if current_node.data == value
-
-    current_node.left = new_node if value < current_node.data
-    current_node.right = new_node if value > current_node.data
-
+   if value < node.data
+      if !node.left #?there's probably a better way to deal wit that instead of nesting if statements
+        node.left = Node.new(value)
+      else
+        node.left = insert(node.left,value)
+      end
+   end
+   return node
   end
+
+  # def insert_non_rec(value) #not working yet
+  #   return "Wrong input!" if value.class != Integer
+  #   current_node = @root
+  #   new_node = Node.new(value)
+
+  #   until !current_node.left && !current_node.right
+  #     current_node = current_node.left if value < current_node.data
+  #     current_node = current_node.right if value > current_node.data
+  #   end
+
+  #   p "This value already exists" if current_node.data == value
+
+  #   current_node.left = new_node if value < current_node.data
+  #   current_node.right = new_node if value > current_node.data
+
+  # end
 
   def delete(value)
 
   end
 
-  def find
-
+  def find(value)
+    #?Maybe while making insert, make it return the node if it already exists and use it here
   end
 
   def level_order
--- main.rb
+++ main.rb
@@ -4,11 +4,15 @@ require_relative 'lib/tree'
 arr = [1,6,6,4,3,4,5,2,3,45,6,7,8,99,9,12]
 #     [1,2,3,4,5,6,7,8,9,12,45,99]
 tree = Tree.new(arr)
-p tree
+tree.insert(-9)
 p tree.pretty_print
- tree.insert(100)
+tree.insert(5)
 p tree.pretty_print
- tree.insert(5)
+tree.insert(100) #it deletes the right of 45
 p tree.pretty_print
- tree.insert(-9)
+tree.insert(101)
+p tree.pretty_print
+tree.insert(26)
+p tree.pretty_print
+tree.insert(-102)
 p tree.pretty_print
